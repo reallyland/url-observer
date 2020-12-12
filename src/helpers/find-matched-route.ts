c63ed7d2e24594c13e3d07a339732558eaaf0af3
Rong Sen Ng <wes.ngrongsen@gmail.com>
2020-12-12 23:34:33 +0800

    refactor: Refactor findMatchedRoute(...)

--- src/helpers/find-matched-route.ts
+++ src/helpers/find-matched-route.ts
@@ -1,18 +1,38 @@
-import type { Routes, RouteValue } from '../custom_typings.js';
+import type { FindMatchedRouteOption, FindMatchedRouteResult, Routes, RouteValue } from '../custom_typings.js';
 
-export function findMatchedRoute(routes: Routes, pathname: string): undefined | RouteValue {
+export function findMatchedRoute<T extends Record<string, unknown>>(
+  routes: Routes,
+  {
+    matcherCallback,
+    scope,
+    status,
+    url: {
+      href,
+      pathname,
+    },
+  }: FindMatchedRouteOption
+): FindMatchedRouteResult<T> {
   let len = pathname.length;
   let match: undefined | RouteValue;
 
   for (const [, routeValue] of routes) {
     if (routeValue.pathRegExp.test(pathname)) {
       len = pathname.replace(routeValue.pathRegExp, '').length;
-
-      if (!len) return routeValue;
-
       match = routeValue;
+
+      if (!len) break;
     }
   }
 
-  return match;
+  const found = match != null;
+
+  return {
+    beforeRouteHandlers: match?.beforeRouteHandlers,
+    found,
+    params: match && matcherCallback ? matcherCallback<T>(pathname, match.pathRegExp) : {} as T,
+    pathRegExp: match?.pathRegExp,
+    scope,
+    status,
+    url: href,
+  };
 }
--- src/tests/helpers/find-matched-route.test.ts
+++ src/tests/helpers/find-matched-route.test.ts
@@ -1,7 +1,8 @@
 import { assert } from '@esm-bundle/chai';
 
-import type { Routes } from '../../custom_typings.js';
+import type { FindMatchedRouteResult, RouteEvent, Routes } from '../../custom_typings.js';
 import { findMatchedRoute } from '../../helpers/find-matched-route.js';
+import { urlParamMatcher } from '../../helpers/url-param-matcher.js';
 
 type A = 'page' | 'page2' | 'subPage' | 'section' | 'subSection';
 
@@ -23,7 +24,9 @@ describe('find-matched-route', () => {
   });
 
   it('matches longest route', () => {
-    const result: [string, undefined | RegExp][] = [];
+    type B = Partial<Record<Exclude<A, 'page' | 'page2'>, string>>;
+
+    const result: [string, FindMatchedRouteResult<B>][] = [];
     const urls: string[] = [
       '/test',
       '/test/123',
@@ -37,21 +40,105 @@ describe('find-matched-route', () => {
     ];
 
     for (const n of urls) {
-      const match = findMatchedRoute(routesMap, n);
+      const match = findMatchedRoute<B>(routesMap, {
+        matcherCallback: urlParamMatcher,
+        scope: '',
+        status: 'manual',
+        url: new URL(n, 'https://example.com'),
+      });
 
-      result.push([n, match?.pathRegExp]);
+      result.push([n, match]);
     }
 
+    const defaultExpected: FindMatchedRouteResult<B> = {
+      beforeRouteHandlers: new Map(),
+      found: true,
+      params: {} as B,
+      pathRegExp: undefined,
+      scope: '',
+      status: 'manual',
+      url: '',
+    };
     assert.deepEqual(result, [
-      ['/test', allRoutes.page],
-      ['/test/123', allRoutes.subPage],
-      ['/test/123/456', allRoutes.section],
-      ['/test/123/456/789', allRoutes.subSection],
-      ['/test/123/456/789/0ab', allRoutes.page2],
-      ['/test-1', allRoutes.page2],
-      ['/test-1/123', allRoutes.page2],
-      ['/page', undefined],
-      ['/page/123', undefined],
+      [
+        '/test',
+        {
+          ...defaultExpected,
+          pathRegExp: allRoutes.page,
+          url: 'https://example.com/test'
+        }
+      ],
+      [
+        '/test/123',
+        {
+          ...defaultExpected,
+          params: { subPage: '123' },
+          pathRegExp: allRoutes.subPage,
+          url: 'https://example.com/test/123'
+        }
+      ],
+      [
+        '/test/123/456',
+        {
+          ...defaultExpected,
+          params: { section: '456', subPage: '123' },
+          pathRegExp: allRoutes.section,
+          url: 'https://example.com/test/123/456',
+        }
+      ],
+      [
+        '/test/123/456/789',
+        {
+          ...defaultExpected,
+          params: { section: '456', subPage: '123', subSection: '789' },
+          pathRegExp: allRoutes.subSection,
+          url: 'https://example.com/test/123/456/789',
+        }
+      ],
+      [
+        '/test/123/456/789/0ab',
+        {
+          ...defaultExpected,
+          pathRegExp: allRoutes.page2,
+          url: 'https://example.com/test/123/456/789/0ab',
+        }
+      ],
+      [
+        '/test-1',
+        {
+          ...defaultExpected,
+          pathRegExp: allRoutes.page2,
+          url: 'https://example.com/test-1',
+        }
+      ],
+      [
+        '/test-1/123',
+        {
+          ...defaultExpected,
+          pathRegExp: allRoutes.page2,
+          url: 'https://example.com/test-1/123',
+        }
+      ],
+      [
+        '/page',
+        {
+          ...defaultExpected,
+          beforeRouteHandlers: undefined,
+          found: false,
+          pathRegExp: undefined,
+          url: 'https://example.com/page',
+        }
+      ],
+      [
+        '/page/123',
+        {
+          ...defaultExpected,
+          beforeRouteHandlers: undefined,
+          found: false,
+          pathRegExp: undefined,
+          url: 'https://example.com/page/123',
+        }
+      ],
     ]);
   });
 
